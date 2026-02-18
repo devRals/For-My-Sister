d0bcfb359380109a349bbd473fb1ba0ab8a76cd4
developerRalsei <fluffycutegoatboi@gmail.com>
2026-02-18 21:45:22 +0300

    I FUCKING HATE WORKFLOWS!

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react"
 
-export default defineConfig({
-  plugins: [],
-});
+export default defineConfig(env => ({
+    plugins: [react()],
+    base: env.mode === "production" ? "/For-My-Sister" : ""
+}));
